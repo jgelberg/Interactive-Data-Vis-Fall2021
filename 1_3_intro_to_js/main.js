00b48d01a362dfa15854ff5b8c3b9a4ded82d801
efrymire <ellie.frymire@gmail.com>
2021-09-02 22:48:43 -0400

    week 1

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,43 @@
-console.log('hello world');
+// console.log('hello world');
+
+// /* string */
+// const myName = "ellie";
+// let neighborhood = "park slope";
+
+// /* numbers */
+// const favNumber = 13;
+
+// /* boolean */ 
+// const cool = true;
+
+// /* arrays */
+// const myArray = [1, 2, 3, 4] // set
+// const mixed = [1, "thing", 2]
+
+// /* object */
+// const myObject = {
+//   name: "ellie",
+//   zip: 11215,
+// } // dictionary
+
+
+// const newArray = myArray.map(data => data + " thing")
+// const newArray = myArray.forEach(data => console.log(data))
+// console.log('myArray :>> ', myArray);
+// console.log('newArray :>> ', newArray);
+
+// const filtered = myArray.filter(booger => booger % 2 === 0)
+// console.log('filtered :>> ', filtered);
+// console.log('filtered.length :>> ', filtered.length);
+
+
+const label = document.getElementById("name-label")
+const submit = document.getElementById("name-submit")
+
+function updateName() {
+  const username = document.getElementById("name-input").value
+  // window.alert("Hello " + document.getElementById("name-input").value + ",welcome to class")
+  window.alert(`Hello ${username}, welcome to class`)
+  label.innerText = `Your name is ${username}. Change it here:`
+  submit.innerText = "Change"
+}
