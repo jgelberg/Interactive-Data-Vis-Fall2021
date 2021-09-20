b5d5ae0096a9a3e2ef78f2c02f4444279d18f0b3
JGELBERG <jamiegelberg@gmail.com>
2021-09-19 21:40:05 -0400

    commit 9/19 9:40pm

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+let counter = 0;
+
+/*increase by 1 */
+function incrementClick() {
+    updateDisplay(++counter);
+}
+
+/* value is set back to zero */
+function resetCounter() {
+    counter = 0;
+    updateDisplay(counter);
+}
+
+/* display the value in the label id */
+function updateDisplay(val) {
+    document.getElementById("label").innerHTML = val;
+}
