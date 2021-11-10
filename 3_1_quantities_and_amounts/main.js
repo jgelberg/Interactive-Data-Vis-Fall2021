e4c3bdebf8939709ef650266960eba5320718462
Jgelberg <jamiegelberg@gmail.com>
2021-11-10 15:01:08 -0500

    commit 11/10 3pm

--- 3_1_quantities_and_amounts/main.js
+++ 3_1_quantities_and_amounts/main.js
@@ -1,40 +1,111 @@
+
+/* https://data.cityofnewyork.us/Education/2010-2011-School-Attendance-and-Enrollment-Statist/7z8d-msnt*/
+
+
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
 
-// // since we use our scales in multiple functions, they need global scope
-// let xScale, yScale;
-
-/* APPLICATION STATE */
-let state = {
-  // data: [],
-};
+const margin = { top: 20, right: 100, bottom: 40, left: 160 }
+const width = 1600 - margin.right - margin.left
+const height = 600 - margin.top - margin.bottom;
 
 /* LOAD DATA */
-d3.csv('[PATH_TO_YOUR_DATA]', d3.autoType).then(raw_data => {
-  console.log("data", raw_data);
-  // save our data to application state
-  state.data = raw_data;
-  init();
-});
 
-/* INITIALIZING FUNCTION */
-// this will be run *one time* when the data finishes loading in
-function init() {
-  /* SCALES */
+d3.csv("../data/2010_-_2011_Enrollement_by_District.csv", d3.autoType)
+  .then(data => {
+    console.log(data);
 
 
+    //const YTD_Enrollment_Avg = data.map(d => d.YTD_Enrollment_Avg)
+    // console.log('YTD_Enrollment_Avg :>> ', YTD_Enrollment_Avg);
+    const svg = d3.select("#container")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .style("background-color", "#f1f1f1")
+      .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  draw(); // calls the draw function
-}
+    const toolTip = d3.select("#container")
+      .append("div")
+      .style("opacity", 0)
+      .style("position", "absolute")
+      .attr("class", "tooltip")
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "2px")
+      .style("border-radius", "5px")
+      .style("padding", "5px")
 
-/* DRAW FUNCTION */
-// we call this every time there is an update to the data/state
-function draw() {
-  /* HTML ELEMENTS */
- 
+     
+
+    const mouseover = function (event, d) {
+      toolTip
+        .style("opacity", 1)
+    }
+    const mousemove = function (event, d) {
+      toolTip
+        .html(
+          `<div>District: ${d.District}</div>
+           <div>Avg Enrollment: ${d.YTD_Enrollment_Avg}</div>`
+        )
+        .style("left", `${event.layerX}px`)
+        .style("top", `${event.layerY}px`)
+
+    }
+    const mouseleave = function (event, d) {
+      toolTip
+        .style("opacity", 0)
+    }
 
 
-}
+    /*SCALES */
+    /** This is where you should define your scales from data to pixel space */
+    const xScale = d3.scaleLinear()
+      .domain([0, 65000])
+      .range([0, width])
+    svg.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(xScale))
+      .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end")
+
+    const yScale = d3.scaleBand()
+      .domain(data.map(d => d.District))
+      .range([0, height])
+      .padding(.1);
+    svg.append("g")
+      .attr("class", "y-axis")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisLeft(yScale))
+      .style("transform", `translate(0px,0px)`)
+
+    const colorScale = d3.scaleSequential()
+      .domain([0, 65000])
+      .interpolator(d3.interpolateRainbow)
+      
+
+
+    svg.selectAll(".bar")
+      .data(data)
+      .join("rect")
+      .attr("x", xScale(0))
+      .attr("y", d => yScale(d.District))
+      .attr("width", d => xScale(d.YTD_Enrollment_Avg))
+      .attr("height", yScale.bandwidth())
+      .attr("fill", function(d, i) { return colorScale(i); })
+      
+      
+      /*.style('fill', function(d) {
+        return colorScale(d);
+      })    */
+      .on("mouseover", (ev,d) =>
+      console.log(ev))
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+     .on("mouseleave", mouseleave)
+  });
+
+
+
+
